6a52028504256f7c3d0460f025ac8c38e601c088
sujeet kumar <sujeetkr503@gmail.com>
2025-06-09 21:21:04 +0530

    basic

---
+++ basic.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+int main()
+{
+    //  Swap two number
+    //    int a=10;
+    //    int b=15;
+    //    int temp;
+    //    temp=b;
+    //    b=a;
+    //    a=temp;
+    //    cout<<a<<" "<<b;
+
+    // Constants
+    //    int a=10;
+    //    a=20;
+    //   cout<<a;
+
+    //   naming convention
+    //    int file_name=0;
+
+    // increment and decrement expression
+    // int a=5;
+    // // ++a;
+    // cout << a++;
+
+    // Example
+    //     float sales=5000;
+    //     float tax=0.05;
+    //     float tax2=0.04;
+    //     float tax_state=sales*tax;
+    //     cout<<tax_state<<endl;
+    //     float tax_country=sales*tax2;
+    //     cout<<tax_country<<endl;
+    //    float remaining_ammount=(sales-(tax_country+tax_state));
+    //     cout << remaining_ammount;
+    //     // 2nd example
+    //     cout<< "Enter Two Number"<<endl;
+    //     int x;
+    //     int y;
+    //     cin>>x>>y;
+    //     cout<<x+y<<endl;
+
+    //  3rd example
+    // cout<<"Enter temperature in Celcius"<<endl;
+    // int temp;
+    // cin>>temp ;
+    // float fahrenheit=(temp*9/5)+32;
+    // cout<<"Fahrenheit = "<<fahrenheit<<endl;
+
+    // 4th Example
+    // double result=floor(1.2);
+    // double result=ceil(1.2);
+    // cout<<"Result = "<<result<<endl;
+
+    // Area of circle
+    // cout<<"Enter Radius of circle"<<endl;
+    // double radius;
+    // cin>>radius;
+    // double area=3.14*(pow(radius,2));
+    // cout<<"Area of circle = "<<area<<endl;
+
+    // char  letter='a';
+    // // cout<<letter;
+    // int number = 0xff ;
+    // cout<<number;
+
+     srand(time(0));
+    int number=rand() % 10;
+        cout<< number << endl;
+
+    return 0;
+}
