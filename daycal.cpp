d95f2188549db43d40669d7892733f42beda08a0
sujeet kumar <sujeetkr503@gmail.com>
2025-06-11 11:58:17 +0530

    previous day calculator

---
+++ daycal.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter any number to count day before n Days.\n";
+    cin>>n;
+    int d;
+    cout<<"Enter today days 0(Sunday) to 7(Saturday) \n";
+    cin>>d;
+
+    int x=n% 7;
+    int ans = d - x;
+
+    if(ans > 0){
+        if(ans == 1){
+            cout<<"Monday";
+        }else if(ans == 2){
+            cout<<"Tuesday";
+        }else if(ans == 3){
+            cout<<"Wednesday";
+        }else if(ans == 4){
+            cout<<"Thursday";
+        }else if(ans == 5){
+            cout<<"Friday";
+        }else if(ans == 6){
+            cout<<"saturday";
+        }else {
+            cout<<"sunday";
+        }
+        // cout<<ans;
+    } else{
+        // cout <<ans +7;
+         if(ans+7 == 1){
+            cout<<"Monday";
+        }else if(ans+7 == 2){
+            cout<<"Tuesday";
+        }else if(ans+7 == 3){
+            cout<<"Wednesday";
+        }else if(ans+7 == 4){
+            cout<<"Thursday";
+        }else if(ans+7 == 5){
+            cout<<"Friday";
+        }else if(ans+7 == 6){
+            cout<<"saturday";
+        }else {
+            cout<<"sunday";
+        }
+    }
+
+}
