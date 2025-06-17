2ac992b30d9fcf13c79d7f26f73f28be5729320c
sujeet kumar <sujeetkr503@gmail.com>
2025-06-17 08:57:40 +0530

    Add average function and main program to calculate the average of an array

---
+++ Exception/average.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std ;
+
+int average(int arr[], int n)
+{
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+        sum = sum + arr[i];
+    int res = sum / n; // Division operation
+    return res;
+}
+
+int main()
+{
+    int n; 
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << average(arr, n);
+    return 0;
+}
