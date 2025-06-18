0b250f8c0f9447314337f97a1e2ee44e2e89d63c
sujeet kumar <sujeetkr503@gmail.com>
2025-06-18 09:20:55 +0530

    Add examples demonstrating pair usage and comparison in C++

---
+++ Pair/comparisonPair.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    pair<int, int> p1(1, 12), p2(9, 12);
+    cout << (p1 == p2) << " "
+         << (p1 != p2) << " "
+         << (p1 > p2) << " "
+         << (p1 < p2) << endl;
+    return 0;
+}
