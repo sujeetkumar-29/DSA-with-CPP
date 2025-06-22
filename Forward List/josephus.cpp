9d9b53bfb563c43c56655f94680b40503dfd4773
sujeet kumar <sujeetkr503@gmail.com>
2025-06-22 11:14:56 +0530

    Add implementation of Josephus problem using list

---
+++ Forward List/josephus.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <list>
+using namespace std;
+
+int getSurv (int n, int k)
+{
+  list<int> l;
+  for (int i=0; i<n; i++)
+    l.push_back(i);
+  auto it = l.begin();
+  while (l.size() > 1)
+  {
+    for (int count = 1; count < k; count++)
+    {
+      it++;
+      if (it == l.end())
+        it = l.begin();
+    }
+    it = l.erase(it);
+    if (it == l.end())
+      it = l.begin();
+  }
+  return *l.begin();
+}
+
+
+int main()
+{
+    cout << getSurv(7, 2);
+    return 0;
+}
