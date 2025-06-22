efb3d356bf9c1dfa31855142c4a4adfa27574dce
sujeet kumar <sujeetkr503@gmail.com>
2025-06-22 11:33:36 +0530

    Add implementation of list operations including insert, print, and replace

---
+++ Forward List/dsStructure.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <list>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+list<int> l;
+
+void insert(int x) { l.push_back(x); }
+
+void print()
+{
+    for(int x : l)
+        cout << x << " ";
+    cout << endl;
+}
+
+void replace(int x, vector<int> &seq)
+{
+    auto it = find(l.begin(), l.end(), x);
+    if(it == l.end())
+        return;
+    it =  l.erase(it);
+    l.insert(it, seq.begin(), seq.end());
+    
+}
+
+int main()
+{
+    insert(3);
+    insert(10);
+    insert(2);
+    insert(10);
+    print();
+    vector<int> seq = {20, 30, 40};
+    replace(10, seq);
+    print();
+    return 0;
+}
